64e74d08bf8209d008c701354d24cc745b2927c3
amelia <amelia.l.sun@pwc.com>
2019-05-07 16:37:29 +0800

    project building

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  greeting: string;
+  // constructor(message: string) {
+  //   this.greeting = message;
+  // }
+  // greet() {
+  //   return "Hello, " + this.greeting;
+  // }
+}
